bbff81db28ed32641cb0a71a49f112c0144e116e
KirigayaDev <valerasaratov064@gmail.com>
2025-10-29 22:15:06 +0300

    Настройка SSL в Postgres

---
+++ configurations/custom_entrypoints/postgres/init_entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chown -R postgres:postgres /var/lib/postgresql/ssl
+chmod 600 /var/lib/postgresql/ssl/*.key
+chmod 644 /var/lib/postgresql/ssl/*.crt
+
+exec docker-entrypoint.sh postgres -c config_file=/etc/postgresql/postgresql.conf
