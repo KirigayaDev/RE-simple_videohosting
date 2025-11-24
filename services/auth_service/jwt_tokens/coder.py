2b3e469cb60101ebba95a45552c5931c955613d0
KirigayaDev <valerasaratov064@gmail.com>
2025-11-24 23:28:25 +0300

    Revert "небольшая оптимизация" (Эта оптимизация явно повышает нагрузку на БД, хоть и прибавит в скорости кода при малых нагрузках) This reverts commit 278622c6c10c96c90e47fe9436aabad4148147c4.

--- services/auth_service/jwt_tokens/coder.py
+++ services/auth_service/jwt_tokens/coder.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import jwt
 
 from configurations import jwt_settings
@@ -29,13 +27,8 @@ def decode_token(token: str) -> TokenPayloadSchema | None:
 
 async def get_token_data(token: str) -> TokenPayloadSchema | None:
     decoded: TokenPayloadSchema = decode_token(token)
-    if decoded is None:
-        return None
-    is_blacklist, version_exists = await asyncio.gather(token_is_blacklisted(decoded),
-                                                        user_and_token_version_exists(decoded.sub,
-                                                                                      decoded.token_version))
-
-    if is_blacklist or not version_exists:
+    if decoded is None or await token_is_blacklisted(decoded) or \
+            not await user_and_token_version_exists(decoded.sub, decoded.token_version):
         return None
 
     return decoded
