35d8f2dd875c3e1a6ea071a6f2e79c4040aa997e
KirigayaDev <valerasaratov064@gmail.com>
2025-12-11 15:34:43 +0300

    инициализация микросервиса

--- services/channel_actions/rabbitmq_app/confirm_video_hls_converting/__init__.py
+++ services/channel_actions/rabbitmq_app/confirm_video_hls_converting/__init__.py
@@ -1,2 +1,1 @@
-from . import _schema
 from . import consumer
---
+++ services/file_upload/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+from healthcheck import router as health_router
+from rabbitmq_app.router import router as rabbit_router
+
+from configurations import development_mode_settings
+
+app = FastAPI(docs_url="/docs" if development_mode_settings.development_mode else None,
+              redoc_url="/redoc" if development_mode_settings.development_mode else None)
+
+app.include_router(health_router)
+app.include_router(rabbit_router)
+
+
+def main():
+    uvicorn.run("main:app",
+                host='0.0.0.0',
+                port=8010,
+                reload=False,
+                ssl_keyfile="/channel_actions/certs/server.key",
+                ssl_certfile="/channel_actions/certs/server.crt",
+                ssl_ca_certs="/channel_actions/certs/rootCA.crt")
+
+
+if __name__ == '__main__':
+    main()
