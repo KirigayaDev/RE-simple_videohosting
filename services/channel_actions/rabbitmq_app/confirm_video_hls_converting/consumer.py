57cce4284b7ccaca482ee1805b4eecdd093de709
KirigayaDev <valerasaratov064@gmail.com>
2025-12-25 01:41:05 +0300

    использование pydantic модели вместо голого json

--- services/channel_actions/rabbitmq_app/confirm_video_hls_converting/consumer.py
+++ services/channel_actions/rabbitmq_app/confirm_video_hls_converting/consumer.py
@@ -5,7 +5,7 @@ from sqlalchemy import update
 from database.session import async_session
 from database.models.video_info import VideoInfo
 
-from ._schema import ConfirmVideoHlsConverting
+from ._schemas import ConfirmVideoHlsConverting
 from ..queues import confirm_video_hls_converting_queue
 
 from ..router import router
--- services/channel_actions/rabbitmq_app/unprocessed_video_upload/consumer.py
+++ services/channel_actions/rabbitmq_app/unprocessed_video_upload/consumer.py
@@ -1,13 +1,11 @@
 import uuid
 
-import orjson
-
 from faststream import AckPolicy
 
 from database.session import async_session
 from database.models.video_info import VideoInfo
 
-from ._schema import UnprocessedVideoUploaded
+from ._schemas import UnprocessedVideoUploaded, ConvertVideoToHls
 from ..queues import unprocessed_video_uploaded_queue, convert_video_to_hls_queue
 
 from ..router import router
@@ -22,4 +20,4 @@ async def handle_unprocessed_video_uploaded(info: UnprocessedVideoUploaded) -> b
         session.add(video_info_db)
         await session.commit()
 
-    return orjson.dumps({"video_path": info.video_path, "uuid": video_uuid})
+    return ConvertVideoToHls(video_uuid=video_uuid, video_path=info.video_path)
