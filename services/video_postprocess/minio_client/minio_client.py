2c729a7ffa4f169a2a489d9b9dad362b3775b56b
KirigayaDev <valerasaratov064@gmail.com>
2025-12-25 16:25:34 +0300

    временная заглушка для просмотра видео

--- services/video_postprocess/minio_client/minio_client.py
+++ services/video_postprocess/minio_client/minio_client.py
@@ -1,9 +1,10 @@
+import orjson
 from minio import Minio
 
 from configurations import minio_settings
+from minio.error import S3Error
 
 from ._http_client import http_client
-
 
 minio_client = Minio(
     endpoint="minio:9000",
@@ -13,3 +14,23 @@ minio_client = Minio(
     cert_check=True,
     http_client=http_client,
 )
+
+public_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"AWS": "*"},
+            "Action": ["s3:GetObject"],
+            "Resource": ["arn:aws:s3:::re-simple-videohosting/videos/*"]
+        }
+    ]
+}
+
+try:
+    minio_client.set_bucket_policy(
+        bucket_name="re-simple-videohosting",
+        policy=orjson.dumps(public_policy)
+    )
+except S3Error as e:
+    raise
